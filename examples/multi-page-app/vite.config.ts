2b73f49526e5803d7f17490612e0a97b1f0d25ab
ge3224 <ge3224@gmail.com>
2024-08-02 11:42:27 -0500

    Handle multi-page app entry points

---
+++ examples/multi-page-app/vite.config.ts
@@ -0,0 +1,17 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    manifest: true,
+
+    rollupOptions: {
+      input: {
+        main: "/src/main.tsx",
+        nested: "/src/nested.tsx",
+      },
+    },
+  },
+})
