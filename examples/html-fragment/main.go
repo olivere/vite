c46c41dac38b0dc85ce033cbe464b484b59d284e
ge3224 <ge3224@gmail.com>
2024-08-23 14:35:30 -0500

    feat: add helper func for minimal integration

---
+++ examples/html-fragment/main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"embed"
+	"flag"
+	"fmt"
+	"html/template"
+	"io/fs"
+	"log"
+	"net"
+	"net/http"
+	"os"
+
+	"github.com/olivere/vite"
+)
+
+//go:embed all:dist
+var dist embed.FS
+
+//go:embed index.tmpl
+var goIndex embed.FS
+
+func main() {
+	var (
+		isDev = flag.Bool("dev", false, "run in development mode")
+	)
+	flag.Parse()
+
+	// The following block sets up the environment and configuration for a Go
+	// application integrated with Vite. It determines whether the application
+	// should run in development or production mode based on the 'isDev' flag
+
+	var viteAssetsDir string
+	var viteAssetsURL string
+	var viteFS fs.FS
+
+	if *isDev {
+		viteAssetsDir = "src/assets"
+		viteAssetsURL = "/src/assets/"
+		viteFS = os.DirFS(".")
+	} else {
+		viteAssetsDir = "dist/assets"
+		viteAssetsURL = "/assets/"
+		fs, err := fs.Sub(dist, "dist")
+		if err != nil {
+			panic(err)
+		}
+		viteFS = fs
+	}
+
+	mux := http.NewServeMux()
+
+	// Serve assets that Vite would treat as 'public' assets.
+	//
+	// In this example, the 'static' directory is used as a replacement for
+	// Vite's default 'public' folder, and 'publicDir' is disabled in the Vite
+	// config. We're using the 'static' directory to achieve similar
+	// functionality, but available to the Go backend and Vite.
+	//
+	// To use a static asset in our Vite frontend, we import it like this:
+	//
+	// import viteLogo from '/static/vite.svg'
+
+	staticAssets := http.FileServer(http.Dir("static"))
+
+	mux.Handle("/static/", http.StripPrefix("/static/", staticAssets))
+
+	// Serve Vite-managed assets from the Go backend, accommodating both
+	// development and production environments.
+	//
+	// Usage in Vite remains the same as in a standard Vite setup. The Go backend
+	// will serve the assets from the correct location based on the environment.
+
+	viteAssets := http.FileServer(http.Dir(viteAssetsDir))
+
+	mux.Handle(viteAssetsURL, http.StripPrefix(viteAssetsURL, viteAssets))
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// viteFragment generates the necessary HTML for Vite integration.
+		//
+		// It calls vite.HTMLFragment with a Config struct to create an HTML fragment
+		// that includes all required Vite assets and scripts. This fragment is intended
+		// to be inserted into the <head> section of the HTML document.
+		viteFragment, err := vite.HTMLFragment(vite.Config{
+			FS:      viteFS,
+			IsDev:   *isDev,
+			ViteURL: "http://localhost:5173",
+		})
+		if err != nil {
+			panic(err)
+		}
+
+		tmpl, err := template.New("index.tmpl").ParseFS(goIndex, "index.tmpl")
+
+		if err != nil {
+			panic(err)
+		}
+
+		if err = tmpl.Execute(w, map[string]interface{}{
+			"Vite": viteFragment,
+		}); err != nil {
+			panic(err)
+		}
+	})
+
+	// Start a listener.
+	l, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		var err1 error
+		if l, err1 = net.Listen("tcp6", "[::1]:0"); err1 != nil {
+			panic(fmt.Errorf("starting HTTP server: %v", err))
+		}
+	}
+
+	// Create a new server.
+	server := http.Server{
+		Handler: mux,
+	}
+
+	log.Printf("Listening on http://%s", l.Addr())
+
+	// Start the server.
+	if err := server.Serve(l); err != nil {
+		panic(err)
+	}
+}
---
+++ fragment.go
@@ -0,0 +1,122 @@
+package vite
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+)
+
+// HTMLFragment generates an HTML fragment for Vite integration based on the provided configuration.
+//
+// This function takes a Config struct and uses it to create the necessary HTML
+// elements for including Vite-managed assets in a web page. The returned HTML
+// is typically intended to be placed in the <head> section of an HTML document.
+//
+// Parameters:
+//   - config: A Config struct containing the necessary configuration options for Vite integration.
+//
+// Returns:
+//   - template.HTML: An HTML fragment as a template.HTML type, which can be safely embedded in HTML templates.
+//   - error: An error if the HTML fragment generation fails for any reason.
+//
+// The returned HTML fragment may include elements such as:
+//   - Script tags for Vite client in development mode
+//   - Links to stylesheets
+//   - Module preload tags
+//   - Script tags for entry points and other necessary JavaScript
+//
+// Usage example:
+//
+//	fragment, err := HTMLFragment(myConfig)
+//	if err != nil {
+//	    // Handle error
+//	}
+//	// Use fragment in your HTML template
+func HTMLFragment(config Config) (template.HTML, error) {
+	pd := &pageData{
+		IsDev:     config.IsDev,
+		ViteEntry: config.ViteEntry,
+		ViteURL:   config.ViteURL,
+	}
+
+	if config.IsDev {
+		pd.PluginReactPreamble = template.HTML(PluginReactPreamble(config.ViteURL))
+	} else {
+		mf, err := config.FS.Open(".vite/manifest.json")
+		if err != nil {
+			return "", fmt.Errorf("vite: open manifest: %w", err)
+		}
+		defer mf.Close()
+
+		m, err := ParseManifest(mf)
+		if err != nil {
+			return "", fmt.Errorf("vite: parse manifest: %w", err)
+		}
+		var chunk *Chunk
+		if chunk == nil {
+			if pd.ViteEntry == "" {
+				chunk = m.GetEntryPoint()
+			} else {
+				entries := m.GetEntryPoints()
+				for _, entry := range entries {
+					if pd.ViteEntry == entry.Src {
+						chunk = entry
+						break
+					}
+				}
+			}
+			if chunk == nil {
+				return "", fmt.Errorf("vite: new page data: unable to parse manifest")
+			}
+		}
+
+		pd.StyleSheets = template.HTML(m.GenerateCSS(chunk.Src))
+		pd.Modules = template.HTML(m.GenerateModules(chunk.Src))
+		pd.PreloadModules = template.HTML(m.GeneratePreloadModules(chunk.Src))
+	}
+
+	// Create a buffer to store the executed template output
+	var buf bytes.Buffer
+
+	// Parse the predefined headTmpl into a new template
+	tmpl, err := template.New("vite").Parse(htmlTmpl)
+	if err != nil {
+		// Return an error if parsing fails
+		return "", fmt.Errorf("vite: parse middleware template: %w", err)
+	}
+
+	// Execute the template with pd (PageData) as the data source
+	err = tmpl.Execute(&buf, pd)
+	if err != nil {
+		// Return an error if template execution fails
+		return "", fmt.Errorf("vite: execute middleware template: %w", err)
+	}
+
+	return template.HTML(buf.Bytes()), nil
+}
+
+// htmlTmpl is a constant string that contains a Go template for including
+// Vite-related scripts and stylesheets in a <head> element of an HTML page.
+// This template adapts its output based on whether the application is running
+// in development or production mode.
+const htmlTmpl = `
+{{- if .IsDev }}
+	{{ .PluginReactPreamble }}
+	<script type="module" src="{{ .ViteURL }}/@vite/client"></script>
+	{{- if ne .ViteEntry "" }}
+		<script type="module" src="{{ .ViteURL }}/{{ .ViteEntry }}"></script>
+	{{- else }}
+		<script type="module" src="{{ .ViteURL }}/src/main.tsx"></script>
+	{{- end }}
+{{- else }}
+	{{- if .StyleSheets }}
+	{{ .StyleSheets }}
+	{{- end }}
+	{{- if .Modules }}
+	{{ .Modules }}
+	{{- end }}
+	{{- if .PreloadModules }}
+	{{ .PreloadModules }}
+	{{- end }}
+{{- end }}
+`
